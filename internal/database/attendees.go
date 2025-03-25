2ef57370711100f9a7efe085387b8eb87802b345
Sazid Mahmud <sazidmahmud5@gmail.com>
2025-03-25 16:17:00 +0600

    Update attendees.go

--- internal/database/attendees.go
+++ internal/database/attendees.go
@@ -11,3 +11,8 @@ type Attendee struct {
 	UserId  int `json:"userId"`
 	EventId int `json:"eventId"`
 }
+
+/*
+The Attendee struct includes three fields: Id, UserId, and EventId.
+An attendee is a user that has signed up for an event. An event can have many attendees and an attendee can attend many events.
+*/
