b2ac3df51746ee1309bc1e12e77fc22e83a93286
Sazid Mahmud <sazidmahmud5@gmail.com>
2025-03-26 04:21:49 +0600

    Update events.go

--- internal/database/events.go
+++ internal/database/events.go
@@ -11,11 +11,11 @@ type EventModel struct {
 }
 type Event struct {
 	Id          int    `json:"id"`
-	OwnerId     int    `json:"ownerId" binding:"required"`
 	Name        string `json:"name" binding:"required,min=3"`
 	Description string `json:"description" binding:"required,min=10"`
 	Date        string `json:"date" binding:"required,datetime=2006-01-02"`
 	Location    string `json:"location" binding:"required,min=3"`
+	OwnerId     int    `json:"ownerId"`
 }
 
 /*
