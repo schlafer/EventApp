28b086407d40827de9967ad17b98e07188b69ed2
Sazid Mahmud <sazidmahmud5@gmail.com>
2025-03-26 00:01:29 +0600

    Update routes.go

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,12 +8,23 @@ import (
 
 func (app *application) routes() http.Handler {
 	g := gin.Default()
+	v1 := g.Group("/api/v1")
+	{
+		v1.POST("/events", app.createEvent)
+		v1.GET("/events", app.getAllEvents)
+		v1.GET("/events/:id", app.getEvent)
+		v1.PUT("/events/:id", app.updateEvent)
+		v1.DELETE("/events/:id", app.deleteEvent)
+		v1.POST("/auth/register", app.registerUser)
+	}
+
 	return g
 }
 
 /*
 Routes is a function that initializes a new Gin server instance using gin.Default(),
 which sets up some default middleware (like logging and recovery).
-Currently, it just returns the Gin instance.
-We will add some routes to this instance later.
+We define a route group /api/v1 to version our API.
+Within this group, we map HTTP methods and paths to the corresponding handler functions for event operations.
+This structure helps organize routes and makes it easier to manage API versions.
 */
