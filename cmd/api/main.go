502d5e6e07450227984f2e72b08dd1e63a433a36
Sazid Mahmud <sazidmahmud5@gmail.com>
2025-03-25 16:07:30 +0600

    Update main.go

---
+++ cmd/api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+}
