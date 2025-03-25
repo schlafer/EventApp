b1546f76b498cb5fc24e88529dbea3457c3c43d1
Sazid Mahmud <sazidmahmud5@gmail.com>
2025-03-25 16:16:53 +0600

    Update users.go

--- internal/database/users.go
+++ internal/database/users.go
@@ -12,3 +12,10 @@ type User struct {
 	Name     string `json:"name"`
 	Password string `json:"-"`
 }
+
+/*
+The UserModel struct contains a DB field, which is a pointer to a sql.DB instance.
+The User struct includes four fields: Id, Email, Password, and Name.
+json tags are used to define how the struct fields are converted to and from JSON, ensuring proper data serialization and deserialization.
+The Password field is marked with a - in the json tag, instructing the JSON package to exclude it from JSON responses, making sure we don’t expose the password in the response.
+*/
