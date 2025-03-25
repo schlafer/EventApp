ae8abb4fa70b895318c4c68431bc7161e6efe8d9
Sazid Mahmud <sazidmahmud5@gmail.com>
2025-03-26 04:21:53 +0600

    Update attendees.go

--- internal/database/attendees.go
+++ internal/database/attendees.go
@@ -25,7 +25,7 @@ func (m *AttendeeModel) Insert(attendee *Attendee) (*Attendee, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	query := `INSERT INTO attendees (event_id, user_id) VALUES ($1, $2) RETURNING id`
+	query := "INSERT INTO attendees (event_id, user_id) VALUES ($1, $2) RETURNING id"
 	err := m.DB.QueryRowContext(ctx, query, attendee.EventId, attendee.UserId).Scan(&attendee.Id)
 
 	if err != nil {
@@ -42,46 +42,54 @@ func (m *AttendeeModel) GetByEventAndAttendee(eventId, userId int) (*Attendee, e
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	query := `SELECT * FROM attendees WHERE event_id = $1 AND user_id = $2`
+	query := "SELECT * FROM attendees where event_id = $1 AND user_id = $2"
+
 	var attendee Attendee
 	err := m.DB.QueryRowContext(ctx, query, eventId, userId).Scan(&attendee.Id, &attendee.UserId, &attendee.EventId)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, nil
 		}
+
 		return nil, err
 	}
+
 	return &attendee, nil
 }
 
 //This method retrieves an attendee record from the database based on
 // the provided event ID and user ID.
 
-func (m AttendeeModel) GetAttendeesByEvent(eventId int) ([]User, error) {
+func (m *AttendeeModel) GetAttendeesByEvent(eventId int) ([]*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	query := `
-     SELECT u.id, u.name, u.email
-     FROM users u
-     JOIN attendees a ON u.id = a.user_id
-     WHERE a.event_id = $1
- `
+	 SELECT u.id, u.name, u.email
+	 FROM users u
+	 JOIN attendees a ON u.id = a.user_id
+	 where a.event_id = $1
+	`
+
 	rows, err := m.DB.QueryContext(ctx, query, eventId)
 	if err != nil {
 		return nil, err
 	}
+
 	defer rows.Close()
 
-	var users []User
+	var users []*User
+
 	for rows.Next() {
 		var user User
 		err := rows.Scan(&user.Id, &user.Name, &user.Email)
 		if err != nil {
 			return nil, err
 		}
-		users = append(users, user)
+
+		users = append(users, &user)
 	}
+
 	return users, nil
 }
 
@@ -92,18 +100,20 @@ func (m *AttendeeModel) Delete(userId, eventId int) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	query := `DELETE FROM attendees WHERE user_id = $1 AND event_id = $2`
+	query := "DELETE FROM attendees WHERE user_id = $1 AND event_id = $2"
 	_, err := m.DB.ExecContext(ctx, query, userId, eventId)
 	if err != nil {
 		return err
 	}
+
 	return nil
+
 }
 
 // This method deletes an attendee from an event
 // with the provided user ID and event ID.
 
-func (m EventModel) GetByAttendee(attendeeId int) ([]Event, error) {
+func (m *AttendeeModel) GetEventsByAttendee(attendeeId int) ([]*Event, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
@@ -117,18 +127,21 @@ func (m EventModel) GetByAttendee(attendeeId int) ([]Event, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	defer rows.Close()
 
-	var events []Event
+	var events []*Event
 	for rows.Next() {
 		var event Event
 		err := rows.Scan(&event.Id, &event.OwnerId, &event.Name, &event.Description, &event.Date, &event.Location)
 		if err != nil {
 			return nil, err
 		}
-		events = append(events, event)
+		events = append(events, &event)
 	}
+
 	return events, nil
+
 }
 
 // This method retrieves all events a user is attending
